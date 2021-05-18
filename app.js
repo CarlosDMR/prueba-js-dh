11b662089a5cc3b44be002c03e90fe4a5a6957d4
al159995@alumnos.uacj.mx <Al159995@alumnos.uacj.mx>
2021-05-17 20:54:56 -0600

    Lo siento, batalle bastante para realizarlo :/

--- app.js
+++ app.js
@@ -6,17 +6,21 @@
            * pendiente
          )
 */
+const fs = require('fs');
+const listado = fs.readFileSync('./tareas.json', 'utf-8');
+let lista = JSON.parse(listado);
 
 exports.leerTareas = function () {
-    /*
-        En esta función debes leer y retornar las tareas registradas.
-    */
+    const L = fs.readFileSync('./tareas.json', 'utf-8');
+    return JSON.parse(L)
 }
 
 exports.agregarTarea = function (tarea) {
     /*
         Registra y guarda una nueva tarea.
     */
+    lista.push(tarea)
+    fs.writeFileSync('tareas.json', JSON.stringify(lista))
 }
 
 exports.filtrarTareasPorEstado = function (estado) {
@@ -24,4 +28,11 @@ exports.filtrarTareasPorEstado = function (estado) {
         En esta función debes de leer las tareas y retornar las que tengan el estado que se
         manda en el parametro.
     */
+    let ListStatus = []
+    for(let i = 0; i < lista.length; i++){
+        if(lista[i]['status'] == estado){
+            ListStatus.push(lista[i]);
+        }
+    }
+    return ListStatus;
 }
